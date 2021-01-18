e1ed6f1bf091185b4a1067c224d65890ce9808e4
gertberd <15979373+gertberd@users.noreply.github.com>
2021-01-19 00:33:46 +0300

    Enable filter through new buildings on index page

--- property/views.py
+++ property/views.py
@@ -12,10 +12,10 @@ def format_price(value):
 
 
 def show_flats(request):
-    town = request.GET.get('town')
-    min_price = format_price(request.GET.get('min_price'))
-    max_price = format_price(request.GET.get('max_price'))
-    new_building = request.GET.get('new_building') == "1"
+    town = request.GET.get("town")
+    min_price = format_price(request.GET.get("min_price"))
+    max_price = format_price(request.GET.get("max_price"))
+    new_building = request.GET.get("new_building") == "1"
 
     flats = Flat.objects.all()
     if town:
@@ -24,12 +24,19 @@ def show_flats(request):
         flats = flats.filter(price__gt=min_price)
     if max_price:
         flats = flats.filter(price__lt=max_price)
+    if new_building:
+        flats = flats.filter(new_building=True)
 
-    towns = Flat.objects.values_list('town', flat=True).distinct().order_by('town')
-    return render(request, 'flats_list.html', {
-        'flats': flats[:10],
-        'towns': towns,
-        'active_town': town,
-        'max_price': max_price,
-        'min_price': min_price,
-        'new_building': new_building})
+    towns = Flat.objects.values_list("town", flat=True).distinct().order_by("town")
+    return render(
+        request,
+        "flats_list.html",
+        {
+            "flats": flats[:10],
+            "towns": towns,
+            "active_town": town,
+            "max_price": max_price,
+            "min_price": min_price,
+            "new_building": new_building,
+        },
+    )
