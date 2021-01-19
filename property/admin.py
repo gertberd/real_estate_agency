cc178b7ed9620cb5a951b5d28422186185d13cd1
gertberd <15979373+gertberd@users.noreply.github.com>
2021-01-20 00:02:36 +0300

    Add likes to Flat model

--- property/admin.py
+++ property/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import Flat
+from .models import Flat, Claim
 
 
 class FlatAdmin(admin.ModelAdmin):
+    raw_id_fields = ("liked_by",)
     search_fields = ("town", "address", "owner")
     readonly_fields = ("created_at",)
     list_display = ("address", "price", "new_building", "construction_year", "town")
--- property/models.py
+++ property/models.py
@@ -40,6 +40,9 @@ class Flat(models.Model):
         "Год постройки здания", null=True, blank=True, db_index=True
     )
     new_building = models.NullBooleanField()
+    liked_by = models.ManyToManyField(
+        User, blank=True, verbose_name="Кто лайкнул", related_name="liked_flats"
+    )
 
     def __str__(self):
         return f"{self.town}, {self.address} ({self.price}р.)"
